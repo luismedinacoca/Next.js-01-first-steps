bccfa1b35b794664a3bdc0194a8ca15ed9917481
Luis Medina Coca <luismedinacoca@gmail.com>
2025-11-15 17:00:20 -0300

    section02-lecture015: Additional Routes

---
+++ app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+  return (
+    <>
+      <span className="text-7xl flex justify-center items-center">About Page</span>
+    </>
+  )
+}
---
+++ app/contact/page.tsx
@@ -0,0 +1,7 @@
+export default function ContactPage() {
+  return (
+    <>
+      <span className="text-7xl flex justify-center items-center">Contact Page</span>
+    </>
+  )
+}
