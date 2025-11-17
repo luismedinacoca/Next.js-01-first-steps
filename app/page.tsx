08985ec8ffa96320d37e79edac7e36d15b8eece4
Luis Medina Coca <luismedinacoca@gmail.com>
2025-11-17 14:39:37 -0300

    section02-lecture023: Sum up

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="flex flex-col items-center p-24">
       <span className="text-5xl">Hello World!</span>
+      <Link href="/about" className="text-blue-500 hover:text-blue-400 hover:font-bold">
+        About Page
+      </Link>
     </main>
   );
 }
